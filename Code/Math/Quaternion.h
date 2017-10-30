075304afd24985f7f29f42a3dd3f92e5365281cd
Yaukey <yaukeywang@gmail.com>
2017-10-30 16:20:08 +0800

    Update Quaternion.

--- Code/Math/Quaternion.h
+++ Code/Math/Quaternion.h
@@ -59,20 +59,83 @@ namespace yw
             return Quaternion(x - o.x, y - o.y, z - o.z, w - o.w);
         }
 
-        //inline Quaternion operator *(const Quaternion &o) const
-        //{
+        inline Quaternion operator *(const Quaternion &o) const
+        {
+            Quaternion value;
+            value.x = w * o.x + x * o.w + y * o.z - z * o.y;
+            value.y = w * o.y - x * o.z + y * o.w + z * o.x;
+            value.z = w * o.z + x * o.y - y * o.x + z * o.w;
+            value.w = w * o.w - x * o.x - y * o.y - z * o.z;
 
-        //}
+            return value;
+        }
 
-        //inline Quaternion operator *(const float o) const
-        //{
+        inline Quaternion operator *(const float o) const
+        {
+            Quaternion value(x * o, y * o, z * o, w * o);
+            return value;
+        }
 
-        //}
+        inline Quaternion operator /(const float o) const
+        {
+            float oneOverO = 1.0f / o;
+            Quaternion value(x * oneOverO, y * oneOverO, z * oneOverO, w * oneOverO);
 
-        //inline Quaternion operator /(const float o) const
-        //{
+            return value;
+        }
 
-        //}
+        inline Quaternion& operator +=(const Quaternion &o)
+        {
+            x += o.x;
+            y += o.y;
+            z += o.z;
+            w += o.w;
+
+            return *this;
+        }
+
+        inline Quaternion& operator -=(const Quaternion &o)
+        {
+            x -= o.x;
+            y -= o.y;
+            z -= o.z;
+            w -= o.w;
+
+            return *this;
+        }
+
+        inline Quaternion& operator *=(const Quaternion &o)
+        {
+            Quaternion value;
+            value.x = w * o.x + x * o.w + y * o.z - z * o.y;
+            value.y = w * o.y - x * o.z + y * o.w + z * o.x;
+            value.z = w * o.z + x * o.y - y * o.x + z * o.w;
+            value.w = w * o.w - x * o.x - y * o.y - z * o.z;
+            *this = value;
+
+            return *this;
+        }
+
+        inline Quaternion& operator *=(const float o)
+        {
+            x *= o;
+            y *= o;
+            z *= o;
+            w *= o;
+
+            return *this;
+        }
+
+        inline Quaternion& operator /=(const float o)
+        {
+            float oneOverO = 1.0f / o;
+            x *= oneOverO;
+            y *= oneOverO;
+            z *= oneOverO;
+            w *= oneOverO;
+
+            return *this;
+        }
     };
 }
 
