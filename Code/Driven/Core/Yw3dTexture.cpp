7eca0681950b066eb824d57b722bc82832daef1c
Yaukey <yaukeywang@gmail.com>
2018-05-10 23:42:13 +0800

    Improve texture class.

--- Code/Driven/Core/Yw3dTexture.cpp
+++ Code/Driven/Core/Yw3dTexture.cpp
@@ -4,9 +4,28 @@
 #include "stdafx.h"
 #include "Yw3dTexture.h"
 #include "Yw3dDevice.h"
+#include "Yw3dSurface.h"
 #include "System/Support/YwUtility.h"
 
 namespace yw
 {
+    //Yw3dTexture::Yw3dTexture(Yw3dDevice* device) : 
+    //    IYw3dBaseTexture(device),
+    //    m_MipLevels(0),
+    //    m_fSquaredWidth(0),
+    //    m_fSquaredHeight(0),
+    //    m_MipLevelsData(nullptr)
+    //{
 
+    //}
+
+    //Yw3dTexture::~Yw3dTexture()
+    //{
+    //    for (uint32_t i = 0; i < m_MipLevels; i++)
+    //    {
+    //        YW_SAFE_RELEASE(m_MipLevelsData[i]);
+    //    }
+
+    //    YW_SAFE_DELETE_ARRAY(m_MipLevelsData);
+    //}
 }
