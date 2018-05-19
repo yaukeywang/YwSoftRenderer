33f7cc918f7afaeddaef21461b5c3c2212a5270a
Yaukey <yaukeywang@gmail.com>
2018-05-19 23:30:58 +0800

    Implement Yw3dTexture::SampleTexture.

--- Code/Driven/Core/Yw3dTexture.cpp
+++ Code/Driven/Core/Yw3dTexture.cpp
@@ -104,6 +104,124 @@ namespace yw
 
     Yw3dResult Yw3dTexture::SampleTexture(Vector4& color, float u, float v, float w, const Vector4* xGradient, const Vector4* yGradient, const uint32_t* samplerStates)
     {
+        uint32_t texFilter = samplerStates[Yw3d_TSS_MinFilter];
+        float texMipLevel = 0.0f;
+
+        // Choose proper mip level and filter.
+        if ((nullptr != xGradient) && (nullptr != yGradient))
+        {
+            // Compute the mip-level and determine the texture filter type.
+            const float lenXGrad = xGradient->x * xGradient->x * m_SquaredWidth + xGradient->y * xGradient->y * m_SquaredHeight;
+            const float lenYGrad = yGradient->x * yGradient->x * m_SquaredWidth + yGradient->y * yGradient->y * m_SquaredHeight;
+            const float texelsPerScreenPixel = sqrtf(lenXGrad > lenYGrad ? lenXGrad : lenYGrad);
+
+            if (texelsPerScreenPixel <= 1.0f)
+            {
+                // If texelsPerScreenPixel < 1.0f -> magnification, no mipmapping needed.
+                texMipLevel = 0.0f;
+                texFilter = samplerStates[Yw3d_TSS_MagFilter];
+            }
+            else
+            {
+                // Minification, need mipmapping.
+                static const float invLog2 = 1.0f / logf(2.0f); // Calculate log2.
+                texMipLevel = logf(texelsPerScreenPixel) * invLog2;
+                texFilter = samplerStates[Yw3d_TSS_MinFilter];
+            }
+        }
+
+        const float mipLodBias = *(float*)&samplerStates[Yw3d_TSS_MipLodBias];
+        const float maxMipLevel = *(float*)&samplerStates[Yw3d_TSS_MaxMipLevel];
+        texMipLevel = Clamp(texMipLevel + mipLodBias, 0.0f, maxMipLevel);
+
+        if (Yw3d_TF_Linear == samplerStates[Yw3d_TSS_MipFilter])
+        {
+            uint32_t mipLevelA = ftol(texMipLevel);
+            uint32_t mipLevelB = mipLevelA + 1;
+            mipLevelA = (mipLevelA >= m_MipLevels) ? (m_MipLevels - 1) : mipLevelA;
+            mipLevelB = (mipLevelB >= m_MipLevels) ? (m_MipLevels - 1) : mipLevelB;
+
+            Vector4 colorA;
+            Vector4 colorB;
+            if (Yw3d_TF_Linear == texFilter)
+            {
+                m_MipLevelsData[mipLevelA]->SampleLinear(colorA, u, v);
+                m_MipLevelsData[mipLevelB]->SampleLinear(colorB, u, v);
+            }
+            else
+            {
+                m_MipLevelsData[mipLevelA]->SamplePoint(colorA, u, v);
+                m_MipLevelsData[mipLevelB]->SamplePoint(colorB, u, v);
+            }
+
+            const float interpolation = texMipLevel - (float)mipLevelA;// TODO: Not accurate.
+            Vector4Lerp(color, colorA, colorB, interpolation);
+        }
+        else
+        {
+            uint32_t mipLevel = ftol(texMipLevel);
+            mipLevel = (mipLevel >= m_MipLevels) ? (m_MipLevels - 1) : mipLevel;
+
+            if (Yw3d_TF_Linear == texFilter)
+            {
+                m_MipLevelsData[mipLevel]->SampleLinear(color, u, v);
+            }
+            else
+            {
+                m_MipLevelsData[mipLevel]->SamplePoint(color, u, v);
+            }
+        }
+
+        return Yw3d_S_OK;
+    }
+
+    Yw3dResult Yw3dTexture::GenerateMipSubLevels(uint32_t srcLevel)
+    {
         return Yw3d_E_Unknown;
+    }
+
+    Yw3dResult Yw3dTexture::Clear(uint32_t mipLevel, const Vector4& color, const Yw3dRect* rect)
+    {
+        return Yw3d_E_Unknown;
+    }
+
+    Yw3dResult Yw3dTexture::LockRect(uint32_t mipLevel, void** data, const Yw3dRect* rect)
+    {
+        return Yw3d_E_Unknown;
+    }
+
+    Yw3dResult Yw3dTexture::UnlockRect(uint32_t mipLevel)
+    {
+        return Yw3d_E_Unknown;
+    }
+
+    Yw3dSurface* GetMipLevel(uint32_t mipLevel)
+    {
+        return nullptr;
+    }
+
+    Yw3dFormat Yw3dTexture::GetFormat()
+    {
+        return Yw3d_FMT_R32G32B32A32F;
+    }
+
+    uint32_t Yw3dTexture::GetFormatFloats()
+    {
+        return 0;
+    }
+
+    uint32_t Yw3dTexture::GetMipLevels()
+    {
+        return 0;
+    }
+
+    uint32_t Yw3dTexture::GetWidth(uint32_t)
+    {
+        return 0;
+    }
+
+    uint32_t Yw3dTexture::GetHeight(uint32_t)
+    {
+        return 0;
     }
 }
