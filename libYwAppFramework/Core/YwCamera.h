3b1beb07c12b7bc723d5a3470d1abe61cdc632b4
wangyaoqi <yaukeywang@gmail.com>
2018-07-26 23:44:55 +0800

    Improve camera class.

--- libYwAppFramework/Core/YwCamera.h
+++ libYwAppFramework/Core/YwCamera.h
@@ -15,7 +15,56 @@ namespace yw
         ~Camera();
 
     private:
+        // The graphics class as parent.
+        class Graphics* m_Graphics;
 
+        // The current used render target.
+        Yw3dRenderTarget* m_RenderTarget;
+
+        // If locked surfaces viewport or not.
+        bool m_LockedSurfacesViewport;
+
+        // World matrix.
+        Matrix44 m_WorldMatrix;
+
+        // View matrix.
+        Matrix44 m_ViewMatrix;
+
+        // Projection matrix.
+        Matrix44 m_ProjectionMatrix;
+
+        // Frustum plane.
+        Plane m_Frustum[6];
+
+        // FOV angle.
+        float m_FovAngle;
+
+        // Aspect ratio.
+        float m_Aspect;
+
+        // Near clipping plane.
+        float m_NearClippingPlane;
+
+        // Far clipping plane.
+        float m_FarClippingPlane;
+
+        // View distance.
+        float m_ViewDistance;
+
+        // The camera position.
+        Vector3 m_Position;
+
+        // The camera rotation;
+        Quaternion m_Rotation;
+
+        // The up vector.
+        Vector3 m_Up;
+
+        // The right vector.
+        Vector3 m_Right;
+
+        // The forward vector.
+        Vector3 m_Forward;
     };
 }
 
