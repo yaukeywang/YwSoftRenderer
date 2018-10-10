187e2fc7e81587e6b8b9a9d7b612bba568e3d30e
yaukeywang <yaukeywang@gmail.com>
2018-10-10 14:14:12 +0800

    Update premake5.lua
    Explicitly adding all files.

--- premake5.lua
+++ premake5.lua
@@ -46,19 +46,60 @@ project "libYw3d"
 
     files
     { 
-        "libYw3d/Yw*.h",
-        "libYw3d/Yw*.cpp",
-        "libYw3d/Core/Yw3d*.h",
-        "libYw3d/Core/Yw3d*.cpp",
-        "libYw3d/Math/YwMath*.h",
-        "libYw3d/Math/YwMath*.cpp"
+        "libYw3d/Yw3d.h",
+        "libYw3d/Yw3dBase.h",
+        "libYw3d/Yw3dTypes.h",
+        "libYw3d/YwBase.h",
+
+        "libYw3d/Core/Yw3dBaseShader.h",
+        "libYw3d/Core/Yw3dBaseShader.cpp",
+        "libYw3d/Core/Yw3dBaseTexture.h",
+        "libYw3d/Core/Yw3dBaseTexture.cpp",
+        "libYw3d/Core/Yw3dCore.h",
+        "libYw3d/Core/Yw3dCore.cpp",
+        "libYw3d/Core/Yw3dCubeTexture.h",
+        "libYw3d/Core/Yw3dCubeTexture.cpp",
+        "libYw3d/Core/Yw3dDevice.h",
+        "libYw3d/Core/Yw3dDevice.cpp",
+        "libYw3d/Core/Yw3dIndexBuffer.h",
+        "libYw3d/Core/Yw3dIndexBuffer.cpp",
+        "libYw3d/Core/Yw3dPresentTarget.h",
+        "libYw3d/Core/Yw3dPresentTarget.cpp",
+        "libYw3d/Core/Yw3dPrimitiveAssembler.h",
+        "libYw3d/Core/Yw3dRenderTarget.h",
+        "libYw3d/Core/Yw3dRenderTarget.cpp",
+        "libYw3d/Core/Yw3dShader.h",
+        "libYw3d/Core/Yw3dShader.cpp",
+        "libYw3d/Core/Yw3dSurface.h",
+        "libYw3d/Core/Yw3dSurface.cpp",
+        "libYw3d/Core/Yw3dTexture.h",
+        "libYw3d/Core/Yw3dTexture.cpp",
+        "libYw3d/Core/Yw3dVertexBuffer.h",
+        "libYw3d/Core/Yw3dVertexBuffer.cpp",
+        "libYw3d/Core/Yw3dVertexFormat.h",
+        "libYw3d/Core/Yw3dVertexFormat.cpp",
+        "libYw3d/Core/Yw3dVolume.h",
+        "libYw3d/Core/Yw3dVolume.cpp",
+        "libYw3d/Core/Yw3dVolumeTexture.h",
+        "libYw3d/Core/Yw3dVolumeTexture.cpp",
+
+        "libYw3d/Math/YwMath.h",
+        "libYw3d/Math/YwMathMatrix.h",
+        "libYw3d/Math/YwMathMatrix.inl",
+        "libYw3d/Math/YwMathPlane.h",
+        "libYw3d/Math/YwMathPlane.inl",
+        "libYw3d/Math/YwMathQuaternion.h",
+        "libYw3d/Math/YwMathQuaternion.inl",
+        "libYw3d/Math/YwMathUtility.h",
+        "libYw3d/Math/YwMathVector.h",
+        "libYw3d/Math/YwMathVector.inl"
     }
 
     vpaths 
     {
-        ["*"] = { "libYw3d/Yw*.h", "libYw3d/Yw*.cpp" },
-        ["Core"] = { "libYw3d/Core/Yw3d*.h", "libYw3d/Core/Yw3d*.cpp" },
-        ["Math"] = { "libYw3d/Math/YwMath*.h", "libYw3d/Math/YwMath*.cpp" }
+        ["*"] = { "libYw3d/Yw*.h", "libYw3d/Yw*.inl", "libYw3d/Yw*.cpp" },
+        ["Core"] = { "libYw3d/Core/Yw3d*.h", "libYw3d/Core/Yw3d*.inl", "libYw3d/Core/Yw3d*.cpp" },
+        ["Math"] = { "libYw3d/Math/YwMath*.h", "libYw3d/Math/YwMath*.inl", "libYw3d/Math/YwMath*.cpp" }
     }
 
     filter { "architecture:x86" }
@@ -89,12 +130,25 @@ project "libYwAppFramework"
 
     files
     { 
-        "libYwAppFramework/Yw*.h",
-        "libYwAppFramework/Yw*.cpp",
-        "libYwAppFramework/Core/Yw*.h",
-        "libYwAppFramework/Core/Yw*.cpp",
-        "libYwAppFramework/ThirdParty/*.h",
-        "libYwAppFramework/ThirdParty/*.cpp"
+        --"libYwAppFramework/Yw*.h",
+        --"libYwAppFramework/Yw*.cpp",
+
+        "libYwAppFramework/Core/YwApplication.h",
+        "libYwAppFramework/Core/YwApplication.cpp",
+        "libYwAppFramework/Core/YwBaseApplication.h",
+        "libYwAppFramework/Core/YwBaseApplication.cpp",
+        "libYwAppFramework/Core/YwCamera.h",
+        "libYwAppFramework/Core/YwCamera.cpp",
+        "libYwAppFramework/Core/YwEntity.h",
+        "libYwAppFramework/Core/YwGraphics.h",
+        "libYwAppFramework/Core/YwGraphics.cpp",
+        "libYwAppFramework/Core/YwScene.h",
+        "libYwAppFramework/Core/YwScene.cpp",
+        "libYwAppFramework/Core/YwStateBlock.h",
+        "libYwAppFramework/Core/YwStateBlock.cpp",
+
+        --"libYwAppFramework/ThirdParty/*.h",
+        --"libYwAppFramework/ThirdParty/*.cpp"
     }
 
     vpaths 
