561b2933a762f9b738265b9a97af07480d422d12
yaukeywang <yaukeywang@gmail.com>
2018-10-13 16:50:49 +0800

    Update triangle demo camera position.

--- Demo1Triangle/YwDemoTriangleApp.cpp
+++ Demo1Triangle/YwDemoTriangleApp.cpp
@@ -34,7 +34,7 @@ namespace yw
         m_Camera->CalculateProjection(YW_PI / 6.0f, 4.0f / 3.0f, 1.0f, 1000.0f);
 
         // Calculation view matrix.
-        m_Camera->SetPosition(Vector3(0.0f, 0.0f, -10.0f));
+        m_Camera->SetPosition(Vector3(0.0f, 5.0f, -10.0f));
         m_Camera->SetLookAt(Vector3(0.0f, 0.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f));
         m_Camera->CalculateView();
 
