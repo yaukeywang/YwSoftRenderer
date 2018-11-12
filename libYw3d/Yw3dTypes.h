67b2318491caa85231f1a951362d3b962107edd3
yaukeywang <yaukeywang@gmail.com>
2018-11-12 14:03:12 +0800

    update comment.

--- libYw3d/Yw3dTypes.h
+++ libYw3d/Yw3dTypes.h
@@ -144,14 +144,14 @@ enum Yw3dTextureSamplerState
 	Yw3d_TSS_NumTextureSamplerStates
 };
 
-// Defines the supported texture adressing-modes.
+// Defines the supported texture addressing-modes.
 enum Yw3dTextureAddress
 {
     Yw3d_TA_Wrap,
     Yw3d_TA_Clamp
 };
 
-// Defines the supported texturefilters.
+// Defines the supported texture filters.
 enum Yw3dTextureFilter
 {
     Yw3d_TF_Point,    // Specifies nearest point sampling.
@@ -178,9 +178,9 @@ enum Yw3dFormat
     Yw3d_FMT_R32G32B32F,    // 96-bit texture format, three floats mapped to the three color channel.
     Yw3d_FMT_R32G32B32A32F, // 128-bit texture format, four floats mapped to the three color channel plus the alpha channel.
 
-    // Indexbuffer formats.
-    Yw3d_FMT_INDEX16,       // 16-bit indexbuffer format, indices are shorts.
-    Yw3d_FMT_INDEX32        // 32-bit indexbuffer format, indices are integers.
+    // Index buffer formats.
+    Yw3d_FMT_INDEX16,       // 16-bit index buffer format, indices are shorts.
+    Yw3d_FMT_INDEX32        // 32-bit index buffer format, indices are integers.
 };
 
 // Defines the supported primitive types.
