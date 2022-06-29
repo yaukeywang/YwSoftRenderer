dc3c46d5938dcf1ef97093c03b66fc1e862ebbb5
yaoqiwang <yaoqiwang@tencent.com>
2022-06-29 20:28:04 +0800

    Improving prefilter reflect map generation shader.

--- Demo7PBRIBL/YwDemoPBRIBLShader.cpp
+++ Demo7PBRIBL/YwDemoPBRIBLShader.cpp
@@ -175,6 +175,51 @@ namespace yw
 
     bool DemoPBRIBLPrefilterReflectionMapPixelShader::Execute(const Yw3dShaderRegister* input, Vector4& color, float& depth)
     {
+        // Get world position.
+        const Vector3& worldPos = input[0];
+        const float roughness = GetFloat(0);
+
+        Vector3 N = normalize(worldPos);
+
+        // make the simplyfying assumption that V equals R equals the normal 
+        Vector3 R = N;
+        Vector3 V = R;
+
+        const uint32_t SAMPLE_COUNT = 1024u;
+        Vector3 prefilteredColor = Vector3(0.0f);
+        float totalWeight = 0.0f;
+
+        for (uint32_t i = 0u; i < SAMPLE_COUNT; ++i)
+        {
+            // generates a sample vector that's biased towards the preferred alignment direction (importance sampling).
+            Vector2 Xi = Hammersley(i, SAMPLE_COUNT);
+            Vector3 H = ImportanceSampleGGX(Xi, N, roughness);
+            Vector3 L = normalize(2.0f * dot(V, H) * H - V);
+
+            float NdotL = max(dot(N, L), 0.0f);
+            if (NdotL > 0.0f)
+            {
+                // sample from the environment's mip level based on roughness/pdf
+                float D = DistributionGGX(N, H, roughness);
+                float NdotH = max(dot(N, H), 0.0f);
+                float HdotV = max(dot(H, V), 0.0f);
+                float pdf = D * NdotH / (4.0f * HdotV) + 0.0001f;
+
+                float resolution = 512.0f; // resolution of source cubemap (per face)
+                float saTexel = 4.0f * PI / (6.0f * resolution * resolution);
+                float saSample = 1.0f / (float(SAMPLE_COUNT) * pdf + 0.0001f);
+
+                float mipLevel = (roughness == 0.0f) ? 0.0f : (0.5f * log2(saSample / saTexel));
+
+                //prefilteredColor += textureLod(0, L, mipLevel).rgb * NdotL;
+                prefilteredColor += tex2D(0, 0, L, mipLevel).rgb * NdotL;
+                totalWeight += NdotL;
+            }
+        }
+
+        prefilteredColor = prefilteredColor / totalWeight;
+        color = Vector4(prefilteredColor, 1.0f);
+
         return true;
     }
 
